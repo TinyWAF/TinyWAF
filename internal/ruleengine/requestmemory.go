a817ebead364f141a65ab2a3dc820405da7e9c02
nevada_scout <joe@jckd.co.uk>
2024-10-31 23:57:18 +0100

    Save requests to memory

--- internal/ruleengine/requestmemory.go
+++ internal/ruleengine/requestmemory.go
@@ -3,19 +3,39 @@ package ruleengine
 import (
 	"net/http"
 	"sync"
+	"time"
 )
 
-var memory *sync.Map
-var ipLastRequest *sync.Map
+var ipRequestHistory *sync.Map
 
-type rememberedRequest struct{}
+type rememberedRequest struct {
+	timestamp int64
+	request   *http.Request
+}
 
 func init() {
-	memory = &sync.Map{}
-	ipLastRequest = &sync.Map{}
+	ipRequestHistory = &sync.Map{}
 }
 
 // Save up to 10 previous requests to memory per source IP. Use some kind of
 // limited size array that pushes old items out when new ones are added
 func RememberRequest(r *http.Request) {
+	var requestsForIp []rememberedRequest
+
+	data, ok := ipRequestHistory.Load(r.RemoteAddr)
+	if ok {
+		// Type assertion
+		requestsForIp = data.([]rememberedRequest)
+	} else {
+		// Create an empty slice if this is the first request for this IP
+		requestsForIp = []rememberedRequest{}
+	}
+
+	requestsForIp = append(requestsForIp, rememberedRequest{
+		timestamp: time.Now().Unix(),
+		request:   r,
+	})
+
+	ipRequestHistory.Store(r.RemoteAddr, requestsForIp)
+}
 }
