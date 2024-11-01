33e4d162533de094ebacf4342acc74327e313772
nevada_scout <joe@jckd.co.uk>
2024-11-02 00:11:07 +0100

    Expose loaded rules to package

--- internal/ruleengine/ruleengine.go
+++ internal/ruleengine/ruleengine.go
@@ -2,12 +2,16 @@ package ruleengine
 
 import "github.com/TinyWAF/TinyWAF/internal/config"
 
+var loadedRules *config.Rules
+
 // Start the rule engine. Load rules from files defined in config
 func Init(cfg *config.MainConfig) error {
-	_, err := config.LoadRules(cfg)
+	rules, err := config.LoadRules(cfg)
 	if err != nil {
 		return err
 	}
+
+	loadedRules = &rules
 
 	// @todo: implement a way to reload config without restarting app - like apache can
 
