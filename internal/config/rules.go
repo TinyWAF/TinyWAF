0137ff5c7d7ec199d45d0efc7461d225b16ed65f
nevada_scout <joe@jckd.co.uk>
2024-11-02 00:06:31 +0100

    Define rules struct

--- internal/config/rules.go
+++ internal/config/rules.go
@@ -1,3 +1,33 @@
 package config
 
-type rulesConfiguration struct{}
+import (
+	"errors"
+	"log"
+	"path/filepath"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/spf13/viper"
+)
+
+type Rules struct {
+	RequestRules  []RuleGroup `validate:"required"`
+	ResponseRules []RuleGroup
+}
+
+type RuleGroup struct {
+	Group string `validate:"required"`
+	Rules []Rule `validate:"required"`
+}
+
+type Rule struct {
+	Id        string            `validate:"required"`
+	Inspect   []string          `validate:"required,gt=0"`
+	Operators map[string]string `validate:"required,gt=0"`
+	Ratelimit struct {
+		MaxAllowedRequests int `validate:"omitempty,gt=0"`
+		WithinMinutes      int `validate:"omitempty,gt=0"`
+	}
+	Action string `validate:"required"`
+}
+	return rules, nil
+}
